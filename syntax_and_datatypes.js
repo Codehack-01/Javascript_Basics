287d7a22cf040b130250bc08c477bf44ebc4b82e
Codehack-01 <ademolaprojectdreams@gmail.com>
2023-11-27 16:30:58 +0100

    source code

---
+++ syntax_and_datatypes.js
@@ -0,0 +1,30 @@
+//Multi-line comments in JS, shift+Alt+A
+
+//The if-else statement
+/* syntax: if (condition) {
+    //condition to run if statement is true
+} else {
+    //code to run if condition is false
+} */
+
+if (7 < 5) {
+    console.log('7 is less than 5')
+} else {
+    console.log('This condition is false')
+}
+
+//THE if statement
+/* syntax: if (condition) {
+    // code to run if condition is true
+} */
+
+if (7 > 5) {
+    console.log('7 is less than 5')
+} 
+if (7 < 5) {
+    console.log('7 is less than 5')
+}
+
+typeof(10)
+console.log('My name is Ademola Gbolahan')
+console.log('JavaScript is awesome!') 
